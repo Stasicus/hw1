7973ad9154f6b2597a933aee1ea17303dfdaa5c0
Stasicus <fabek2991@gmail.com>
2023-12-29 14:18:39 +0700

    first commit

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
-//const firstName = 'Stas'
-//firstName = 'Vania'
-//console.log('Hello', firstName)
-
-let firstName = 'Stas'
-firstName = 'Vania'
-console.log('Hello', firstName)
+let firstName = 'Stas Panasiuk'
+firstName = 'Stnislav'
+secondName = 'Panasiuk'
+age = '31 years old'
+hobby = 'Plaing by Guitar, but I have not play nore then two years'
+console.log('Hello', firstName, secondName, age, hobby)
