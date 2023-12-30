b960f8b552b50b0c45e11adca33566558e1b6f38
Stasicus <fabek2991@gmail.com>
2023-12-30 19:51:39 +0700

    first commit

--- app.js
+++ app.js
@@ -1,18 +1,25 @@
-const titleProject = 'Оценка стоимости';
-console.log(typeof titleProject)
-const screensValue = 'шаблонные, с уникальным дизайном, с анимациями';
-console.log(typeof screensValue);
+// const titleProject = 'Оценка стоимости';
+// console.log(typeof titleProject)
+// const screensValue = 'шаблонные, с уникальным дизайном, с анимациями';
+// console.log(typeof screensValue);
 
-const screenPrice = 10000;
-console.log(typeof screenPrice)
+// const screenPrice = 10000;
+// console.log(typeof screenPrice)
 
-const bigInt = 90n;
-console.log(typeof bigInt);
+// const bigInt = 90n;
+// console.log(typeof bigInt);
 
-const fullPrice = 5000;
-console.log(typeof fullPrice);
+// const fullPrice = 5000;
+// console.log(typeof fullPrice);
 
-const obj = {
-    responsive: 'Home work 3',
-    isWorked: true
-}
+// const obj = {
+//     responsive: 'Home work 3',
+//     isWorked: true
+// }
+
+let titleProject = 'Оценка стоимости'
+let screensValue = 'шаблонные, с уникальным дизайном, с анимациями'
+let screenPrice = 10000
+let percentage = 90
+let fullPrice = 5000
+let responsive: true
---
+++ lesson03/lesson-03.js
@@ -0,0 +1,6 @@
+let titleProject = 'Оценка стоимости'
+let screensValue = 'шаблонные, с уникальным дизайном, с анимациями'
+let screenPrice = 10000
+let percentage = 90
+let fullPrice = 5000
+let responsive: true
