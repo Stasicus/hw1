2a1abc658a73f2e2feddfcde0548d2dcfae62bdd
Stasicus <fabek2991@gmail.com>
2023-12-30 19:36:22 +0700

    first commit

--- app.js
+++ app.js
@@ -1,31 +1,3 @@
-<<<<<<< HEAD
-// const age = 27;
-// console.log(typeof age)
-// const bigInt = 213123123123n;
-// console.log(typeof bigInt);
-// const firstName = 'Name'
-// console.log(typeof firstName);
-
-// let age2;
-// console.log(age2);
-
-// const obj = {
-//     name: 'Stas',
-//     age: 31,
-//     isWorked: true
-// }
-
-
-// const num = 10
-// const num2 = 5
-
-// console.log(num + num2)
-// console.log(num - num2)
-// console.log(num * num2)
-// console.log(num / num2)
-// console.log(num % num2)
-
-
 const titleProject = 'Оценка стоимости';
 console.log(typeof titleProject)
 const screensValue = 'шаблонные, с уникальным дизайном, с анимациями';
@@ -44,11 +16,3 @@ const obj = {
     responsive: 'Home work 3',
     isWorked: true
 }
-=======
-let firstName = 'Stas Panasiuk'
-firstName = 'Stnislav'
-secondName = 'Panasiuk'
-age = '31 years old'
-hobby = 'Plaing by Guitar, but I have not play nore then two years'
-console.log('Hello', firstName, secondName, age, hobby)
->>>>>>> 7973ad9154f6b2597a933aee1ea17303dfdaa5c0
