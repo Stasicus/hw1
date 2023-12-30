97eb33190b06b3615e517fe09522e7c6b62ecbb2
Stasicus <fabek2991@gmail.com>
2023-12-30 20:58:52 +0700

    lesson03

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
+let titleProject = 'Оценка стоимости'
+let screensValue = 'шаблонные, с уникальным дизайном, с анимациями'
+let screenPrice = 10000
+let percentage = 90
+let fullPrice = 5000
+let responsive = true
+
 // const titleProject = 'Оценка стоимости';
 // console.log(typeof titleProject)
 // const screensValue = 'шаблонные, с уникальным дизайном, с анимациями';
@@ -17,9 +24,4 @@
 //     isWorked: true
 // }
 
-let titleProject = 'Оценка стоимости'
-let screensValue = 'шаблонные, с уникальным дизайном, с анимациями'
-let screenPrice = 10000
-let percentage = 90
-let fullPrice = 5000
-let responsive = true
+
