74936bbd9c5ddd740cfb7c76ae1a69e43b73634e
Stasicus <fabek2991@gmail.com>
2023-12-30 18:34:00 +0700

    first commit

--- app.js
+++ app.js
@@ -1,7 +1,45 @@
-//const firstName = 'Stas'
-//firstName = 'Vania'
-//console.log('Hello', firstName)
+// const age = 27;
+// console.log(typeof age)
+// const bigInt = 213123123123n;
+// console.log(typeof bigInt);
+// const firstName = 'Name'
+// console.log(typeof firstName);
 
-let firstName = 'Stas'
-firstName = 'Vania'
-console.log('Hello', firstName)
+// let age2;
+// console.log(age2);
+
+// const obj = {
+//     name: 'Stas',
+//     age: 31,
+//     isWorked: true
+// }
+
+
+// const num = 10
+// const num2 = 5
+
+// console.log(num + num2)
+// console.log(num - num2)
+// console.log(num * num2)
+// console.log(num / num2)
+// console.log(num % num2)
+
+
+const titleProject = 'Оценка стоимости';
+console.log(typeof titleProject)
+const screensValue = 'шаблонные, с уникальным дизайном, с анимациями';
+console.log(typeof screensValue);
+
+const screenPrice = 10000;
+console.log(typeof screenPrice)
+
+const bigInt = 90n;
+console.log(typeof bigInt);
+
+const fullPrice = 5000;
+console.log(typeof fullPrice);
+
+const obj = {
+    responsive: 'Home work 3',
+    isWorked: true
+}
